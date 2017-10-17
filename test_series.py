19286d551d74b7050d13c233e99fff9b77818b67
Darren-Haynes <darrenhaynes@zoho.com>
2017-10-17 15:14:54 -0700

    added docstrings

--- test_series.py
+++ test_series.py
@@ -1,3 +1,6 @@
+"""Test file to test fibonacci function in series.py file is return the
+   correct values
+"""
 import pytest
 
 FIB_TABLE = [
@@ -17,6 +20,7 @@ FIB_TABLE = [
 
 @pytest.mark.parametrize('n, result', FIB_TABLE)
 def test_fibonacci(n, result):
+    """Check number sent into fibonacci function equals correct results"""
     from series import fibonacci
     assert fibonacci(n) == result
 
