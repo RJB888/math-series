7063ed0058802a2acbfbf0fa9017620a82994cb5
Darren-Haynes <darrenhaynes@zoho.com>
2017-10-17 15:35:34 -0700

    Added test function to test that lucas function is returning correct values

--- test_series.py
+++ test_series.py
@@ -1,4 +1,4 @@
-"""Test file to test fibonacci function in series.py file is return the
+"""Test file to test fibonacci function, lucas function in series.py file is return the
    correct values
 """
 import pytest
@@ -18,9 +18,29 @@ FIB_TABLE = [
     ]
 
 
+LUCAS_TABLE = [
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 7),
+    (6, 11),
+    (7, 18),
+    (8, 29),
+    (9, 47),
+    (10, 76)
+    ]
+
+
 @pytest.mark.parametrize('n, result', FIB_TABLE)
 def test_fibonacci(n, result):
     """Check number sent into fibonacci function equals correct results"""
     from series import fibonacci
     assert fibonacci(n) == result
 
+
+@pytest.mark.parametrize('n, result', LUCAS_TABLE)
+def test_lucas(n, result):
+    """Check number sent into lucas function equals correct results"""
+    from series import lucas
+    assert lucas(n) == result
