bb6047dff621808bf1b617f96c39b9ee53b9aab4
Darren-Haynes <darrenhaynes@zoho.com>
2017-10-18 11:06:00 -0700

    added tests to test sum_series function

--- test_series.py
+++ test_series.py
@@ -1,11 +1,10 @@
-"""Test file to test fibonacci function, lucas function in series.py file is return the
-   correct values
+"""Test file to test fibonacci function, lucas function, and sum_series
+   function  in series.py file.
 """
 import pytest
 
 FIB_TABLE = [
-    (0, 0),
-    (1, 1),
+    (1, 0),
     (2, 1),
     (3, 1),
     (4, 2),
@@ -19,8 +18,8 @@ FIB_TABLE = [
 
 
 LUCAS_TABLE = [
-    (1, 1),
-    (2, 2),
+    (1, 2),
+    (2, 1),
     (3, 3),
     (4, 4),
     (5, 7),
@@ -30,6 +29,14 @@ LUCAS_TABLE = [
     (9, 47),
     (10, 76)
     ]
+
+SERIES_TABLE = [
+    (0, 0, 0, 0),
+    (1, 4, 4, 15),
+    (4, 262, 42, 110),
+    (10, 490, 12, 7),
+    (10, 144, 2, 3)
+]
 
 
 @pytest.mark.parametrize('n, result', FIB_TABLE)
@@ -44,3 +51,17 @@ def test_lucas(n, result):
     """Check number sent into lucas function equals correct results"""
     from series import lucas
     assert lucas(n) == result
+
+
+@pytest.mark.parametrize('n, result', FIB_TABLE)
+def test_sum_series_for_fibonnaci(n, result):
+    """Test sum_series function works when passed only 1 parameter"""
+    from series import sum_series
+    assert sum_series(n) == result
+
+
+@pytest.mark.parametrize('n, result, pos1, pos2', SERIES_TABLE)
+def test_series(n, result, pos1, pos2):
+    """Check number sent into lucas function equals correct results"""
+    from series import sum_series
+    assert sum_series(n, pos1, pos2) == result
